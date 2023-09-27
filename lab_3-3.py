79fe46c40ab3ee40f20b4d844906a86b84482c4b
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-09-27 11:18:33 -0400

    finished

---
+++ lab_3-3.py
@@ -0,0 +1,15 @@
+
+#Create a Python file named lab_3-3.py
+#Import the random module.
+import random
+#Use the randint() function to generate a random integer between 1 and 100.
+print(random.randint(1,100))
+#Compare your result with another classmate. What do you notice about the results? Write your answers as a comment.
+# that each time it was a different number 
+#Use the seed() function to set the random seed to a value that you and a classmate agree on.
+random.seed (2763)
+#Use the randrange() function to generate an even number between 0 and 100. 
+print(random.randint(1,100))
+#Compare your result with your classmate. What do you notice about the results? Write your answers as a comment.
+# we both got the same anwser 46 
+
